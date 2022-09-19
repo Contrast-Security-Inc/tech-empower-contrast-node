decb3cdb48d33485a0e1cce3a9b788d01a33acc7
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2022-09-19 20:12:08 +0300

    Fix scripts

--- contrast/benchmark-node-protect.sh
+++ contrast/benchmark-node-protect.sh
@@ -20,6 +20,11 @@ fi;
 ./copy-files.sh node-protect $AGENT_FILE
 
 # Update dockerfiles to use correct package
+FILES_TO_MODIFY=($(grep -HRl "/node-contrast.tgz" ../frameworks/JavaScript))
+for i in "${FILES_TO_MODIFY[@]}"
+ do 
+  awk '/npm install .\/node-contrast/ { print; print "RUN pushd ./node_modules/@contrast/mono-workspace && npm install--ignore-scripts--omit-dev && popd"; next }1' ${i} > ${i}.bak && mv ${i}.bak ${i}
+done
 grep -r @contrast/agent ../frameworks/JavaScript -l | xargs -I '{}' -n 1 sed -i.bak "s#@contrast/agent#@contrast/mono-workspace/protect-agent#" {} && ls -d ../frameworks/JavaScript/*/*.bak | xargs -n 1 rm
 
 # Start contrast-service
