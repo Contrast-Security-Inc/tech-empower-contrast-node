e0bdc125c05e850b41101f5a036be3a66a82396f
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2022-09-19 23:22:23 +0300

    Fix scripts

--- contrast/benchmark-node-protect.sh
+++ contrast/benchmark-node-protect.sh
@@ -23,7 +23,7 @@ fi;
 FILES_TO_MODIFY=($(grep -HRl "/node-contrast.tgz" ../frameworks/JavaScript))
 for i in "${FILES_TO_MODIFY[@]}"
  do 
-  awk '/npm install .\/node-contrast/ { print; print "RUN pushd ./node_modules/@contrast/mono-workspace && npm install--ignore-scripts--omit-dev && popd"; next }1' ${i} > ${i}.bak && mv ${i}.bak ${i}
+  awk '/npm install .\/node-contrast/ { print; print "RUN cd ./node_modules/@contrast/mono-workspace && npm install --ignore-scripts --omit-dev && cd "; next }1' ${i} > ${i}.bak && mv ${i}.bak ${i}
 done
 grep -r @contrast/agent ../frameworks/JavaScript -l | xargs -I '{}' -n 1 sed -i.bak "s#@contrast/agent#@contrast/mono-workspace/protect-agent#" {} && ls -d ../frameworks/JavaScript/*/*.bak | xargs -n 1 rm
 
