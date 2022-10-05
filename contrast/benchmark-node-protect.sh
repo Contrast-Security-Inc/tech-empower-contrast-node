eb1675197f99af01d38bac7af90e6203a048a756
Ivaylo Grahlyov <ivaylo.grahlyov@gmail.com>
2022-10-05 13:08:08 +0300

    Fix scripts for modifying Dockerfiles

--- contrast/benchmark-node-protect.sh
+++ contrast/benchmark-node-protect.sh
@@ -20,12 +20,15 @@ fi;
 ./copy-files.sh node-protect $AGENT_FILE
 
 # Update dockerfiles to use correct package
-FILES_TO_MODIFY=($(grep -HRl "/node-contrast.tgz" ../frameworks/JavaScript))
-for i in "${FILES_TO_MODIFY[@]}"
- do 
-  awk '/npm install .\/node-contrast/ { print; print "RUN cd ./node_modules/@contrast/mono-workspace && npm install --ignore-scripts --omit-dev && cd "; next }1' ${i} > ${i}.bak && mv ${i}.bak ${i}
-done
-grep -r @contrast/agent ../frameworks/JavaScript -l | xargs -I '{}' -n 1 sed -i.bak "s#@contrast/agent#@contrast/mono-workspace/protect-agent#" {} && ls -d ../frameworks/JavaScript/*/*.bak | xargs -n 1 rm
+FILES_TO_MODIFY=($(grep -HRl "# Start Contrast Additions for v4 node-agent" ../frameworks/JavaScript))
+if [[ ${FILES_TO_MODIFY[@]} ]]; then
+  for i in "${FILES_TO_MODIFY[@]}"
+  do 
+    awk '/npm install .\/node-contrast/ { print; print "RUN cd ./node_modules/@contrast/mono-workspace && npm install --ignore-scripts --omit-dev && cd "; next }1' ${i} > ${i}.bak && mv ${i}.bak ${i}
+    awk '/Additions for v4 node-agent/ { print "# Start Contrast Additions for v5 node-agent"; next }1' ${i} > ${i}.bak && mv ${i}.bak ${i}
+  done
+  grep -r @contrast/agent ../frameworks/JavaScript -l | xargs -I '{}' -n 1 sed -i.bak "s#@contrast/agent#@contrast/mono-workspace/protect-agent#" {} && ls -d ../frameworks/JavaScript/*/*.bak | xargs -n 1 rm
+fi
 
 # Start contrast-service
 ./start-contrast-service.sh
